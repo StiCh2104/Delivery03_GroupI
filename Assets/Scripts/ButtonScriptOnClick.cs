69e4f5f532b7367a79145938b468d0fec96627e0
Arkantes <arkantes17@gmail.com>
2025-02-25 18:51:38 +0100

    UI title+end scenes
    He hecho la logica de la UI en la escena de title y la de ending

---
+++ Assets/Scripts/ButtonScriptOnClick.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScriptOnClick : MonoBehaviour 
+{
+    public int sceneChanger; //0=start,1=exit;
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        switch (sceneChanger)
+        {
+            case 0: btn.onClick.AddListener(() => Manager.Instance.StartGame()); break;
+            case 1: btn.onClick.AddListener(() => Manager.Instance.EndGame()); break;
+        }
+        
+    }
+}
+
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.Pool;
+using UnityEngine.SceneManagement;
+
+public enum GameScene { Start, Gameplay, Ending }
+public class Manager  : MonoBehaviour 
+{
+    public static Manager Instance { get; private set; }
+    public GameScene CurrentState { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+        }
+
+
+    private void Start()
+    {
+        ChangeState(GameScene.Start);
+    }
+
+    private void ChangeState(GameScene newScene)
+    {
+        CurrentState = newScene;
+        Debug.Log("Cambio de escena a: " + CurrentState);
+        switch (newScene)
+        {
+            case GameScene.Start:
+                SceneManager.LoadScene("Title");
+                break;
+            case GameScene.Gameplay:
+                SceneManager.LoadScene("Gameplay");
+                break;
+            case GameScene.Ending:
+                SceneManager.LoadScene("Ending");
+                break;
+            default: SceneManager.LoadScene("Title");//posible cambio
+                break;
+        }
+    }
+    //procesors to change scenes (call)
+    public void StartGame() { ChangeState(GameScene.Gameplay); }
+    public void GoToMainMenu() { ChangeState(GameScene.Start); }
+    public void EndGame() { ChangeState(GameScene.Ending); }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        if (Input.GetKeyDown(KeyCode.Return))
+            ChangeState(GameScene.Gameplay);
+    }
+
+}
